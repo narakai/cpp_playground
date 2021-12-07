3adbcaff3ea45f58578c2700a3180fa220c75c0f
leon lai <haha@haha.cc>
2021-12-07 13:11:01 +0800

    file

---
+++ file.h
@@ -0,0 +1,14 @@
+//
+// Created by lai leon on 2021/12/7.
+//
+
+#ifndef HELLO_WORLD_FILE_H
+#define HELLO_WORLD_FILE_H
+
+
+class file {
+
+};
+
+
+#endif //HELLO_WORLD_FILE_H
--- student.cpp
+++ student.cpp
@@ -40,12 +40,43 @@
 //    cout << "析构函数!" << endl;
 //}
 
-int main() {
+//继承中 先调用父类构造函数，再调用子类构造函数，析构顺序与构造相反
 
-    Student stu;
-    stu.setName("德玛西亚");
-    stu.setID(250);
-    stu.showStudent();
+//
+//多态满足条件：
+//1、有继承关系
+//2、子类重写父类中的虚函数
+//多态使用：
+//父类指针或引用指向子类对象
 
-    return 0;
-}
+class Animal {
+public:
+    //Speak函数就是虚函数
+    //函数前面加上virtual关键字，变成虚函数，那么编译器在编译的时候就不能确定函数调用了。
+    virtual void speak() {
+        cout << "动物在说话" << endl;
+    }
+};
+
+class Cat : public Animal {
+public:
+    void speak() {
+        cout << "小猫在说话" << endl;
+    }
+};
+
+//​ 1. 虚析构或纯虚析构就是用来解决通过父类指针释放子类对象
+//
+//​ 2. 如果子类中没有堆区数据，可以不写为虚析构或纯虚析构
+//
+//​ 3. 拥有纯虚析构函数的类也属于抽象类
+
+//int main() {
+//
+//    Student stu;
+//    stu.setName("德玛西亚");
+//    stu.setID(250);
+//    stu.showStudent();
+//
+//    return 0;
+//}
